e7a2f407bc9a2ff76cf489ae4e08278485791717
cenkt <orhuncenktaskin@gmail.com>
2022-08-27 13:17:52 +0200

    restructured funcs, prep for part2

--- day16/day16.py
+++ day16/day16.py
@@ -4,8 +4,9 @@ def hex2bin(ch):
         diff -= 7  # to make up for the chars between
     return bin(9 + diff)[2:].zfill(4)
 
+
 def convert_hex_to_bin(msg):
-    while msg[-1] == '0':
+    if msg[-1:] == "\n":
         msg = msg[:-1]
     return "".join([hex2bin(ch) for ch in msg])
 
@@ -27,6 +28,35 @@ def parse_literal_value(msg):
     return msg, binary_string
 
 
+def read_operator_packet(msg, version_sum):
+    length_type_id, body = msg[0], msg[1:]
+    match int(length_type_id, 2):
+        case 0:
+            print(f"length_type_id is 0")
+            subpacket_length, body = int(body[:15], 2), body[15:]
+            print(f"{subpacket_length=}")
+            length_read = 0
+            while length_read != subpacket_length:
+                left, version_sum = read_packet(body, version_sum)
+                length_read += len(body) - len(left)
+                body = left
+        case 1:
+            print(f"length_type_id is 1")
+            subpacket_count, body = int(body[:11], 2), body[11:]
+            print(f"{subpacket_count=}")
+            for i in range(subpacket_count):
+                print(f"{i}th subpkg")
+                body, version_sum = read_packet(body, version_sum)
+    return body, version_sum
+
+
+def read_literal_value(msg):
+    print(f"literal")
+    body, literal_string = parse_literal_value(msg)
+    print(f"return body {body}, {int(literal_string, 2)}")
+    return body, literal_string
+
+
 def read_packet(msg, version_sum=0):
     print(msg)
     print("parsing header")
@@ -35,33 +65,9 @@ def read_packet(msg, version_sum=0):
     print(f"version:{int(version, 2)}")
     print(f"{body:>{len(msg)}}")
     if int(type_id, 2) == 4:
-        print(f"literal")
-        body, literal_string = parse_literal_value(body)
-        print(f"return body {body}, {int(literal_string, 2)}")
-        if len(body) == 0:
-            print("done")
-        elif len(body) < 4:
-            if int(body, 2) == 0:
-                print("just extra 0s, done")
+        body, literal = read_literal_value(body)
     else:
-        length_type_id, body = body[0], body[1:]
-        match int(length_type_id, 2):
-            case 0:
-                print(f"length_type_id is 0")
-                subpacket_length, body = int(body[:15], 2), body[15:]
-                print(f"{subpacket_length=}")
-                length_read = 0
-                while length_read != subpacket_length:
-                    left, version_sum = read_packet(body, version_sum)
-                    length_read += len(body) - len(left)
-                    body = left
-            case 1:
-                print(f"length_type_id is 1")
-                subpacket_count, body = int(body[:11], 2), body[11:]
-                print(f"{subpacket_count=}")
-                for i in range(subpacket_count):
-                    print(f"{i}th subpkg")
-                    body, version_sum = read_packet(body, version_sum)
+        body, version_sum = read_operator_packet(body, version_sum)
     return body, version_sum
 
 
@@ -69,7 +75,6 @@ if __name__ == "__main__":
     input_file = "input.txt"
     with open(input_file, 'r') as f:
         raw_input = f.read()
-
     packet = convert_hex_to_bin(raw_input)
     _, sum_of_vers = read_packet(packet)
     print(f"{sum_of_vers=}")
