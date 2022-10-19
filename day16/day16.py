e619c8cc2eb3fb8aca532631c0f8528c0f2120f7
octaskin <orhuncenktaskin@gmail.com>
2022-10-19 19:16:44 +0200

    finally handled day16

--- day16/day16.py
+++ day16/day16.py
@@ -20,56 +20,55 @@ def parse_literal_value(msg):
     return binary_string, msg
 
 
-def read_operator_packet(msg, read_func):
-    logging.debug(f"reading operator packet \n{msg}")
+def read_operator_packet(msg, is_part1):
     length_type_id, msg = int(msg[0]), msg[1:]
-    logging.debug(f"{length_type_id=}")
     body_start_idx = 15 - length_type_id * 4
     subpacket_len, body = int(msg[:body_start_idx], 2), msg[body_start_idx:]
-    logging.debug(f"subp_len_info:{subpacket_len}")
-    logging.debug(f"{body:>{len(msg) + 1}}")
     subpackets = []
     length_read = 0
     while length_read != subpacket_len:
         initial_len = len(body)
-        value, body = read_func(body)
+        value, body = read_packet(body, is_part1)
         subpackets.append(value)
         match length_type_id:
             case 0:
                 length_read += initial_len - len(body)
             case 1:
                 length_read += 1
-        logging.debug(f"subpkg read:{length_read}/{subpacket_len}")
 
     return subpackets, body
 
 
-def read_packet(msg):
-    logging.debug(f"reading packet \n{msg}")
+def read_packet(msg, is_part1):
     version, type_id, body = msg[:3], msg[3:6], msg[6:]
-    logging.debug(f"version={int(version, 2)}")
-    logging.debug(f"type={int(type_id, 2)}")
-    logging.debug(f"{body:>{len(msg)}}")
     if int(type_id, 2) == 4:
         value, left = parse_literal_value(body)
-        logging.debug(f"literal ret {value, left}")
-        value = int(version, 2)
+        if is_part1:
+            value = version
+        value = int(value, 2)
     else:
-        subpackets, left = read_operator_packet(body, read_packet)
-        value = sum(subpackets) + int(version, 2)
+        subpackets, left = read_operator_packet(body, is_part1)
+        if is_part1:
+            subpackets.append(int(version, 2))
+            type_id = "0"
+
+        match int(type_id, 2):
+            case 0:
+                value = sum(subpackets)
+            case 1:
+                value = math.prod(subpackets)
+            case 2:
+                value = min(subpackets)
+            case 3:
+                value = max(subpackets)
+            case 5:
+                value = int(subpackets[0] > subpackets[1])
+            case 6:
+                value = int(subpackets[0] < subpackets[1])
+            case 7:
+                value = int(subpackets[0] == subpackets[1])
+
     return value, left
-
-
-# def part1_value_calculator(subs, vers, _):
-#     return sum(subs) + int(vers, 2)
-
-
-def initiate_logger(level=logging.WARNING):
-    logger = logging.getLogger('root')
-    logger.setLevel(level)
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(logging.Formatter('%(levelname)s:%(name)s:%(message)s'))
-    logger.addHandler(stream_handler)
 
 
 def main():
@@ -78,10 +77,8 @@ def main():
         raw_input = f.read()
     # raw_input = "A0016C880162017C3686B18A3D4780"
 
-    initiate_logger()
-
     packet = convert_hex_to_bin(raw_input)
-    ret, left = read_packet(packet)
+    ret, _ = read_packet(packet, is_part1=True)
     print(f"{ret=}")
 
 
