28a779ec7a49d99a006d5b24c090b3b8238219b1
octaskin <orhuncenktaskin@gmail.com>
2022-10-19 17:36:35 +0200

    converted print statements to logger

--- day16/day16.py
+++ day16/day16.py
@@ -1,14 +1,19 @@
+import math
+import logging
+import sys
+
+
 def convert_hex_to_bin(msg):
     return "".join(f"{int(a, 16):0>4b}" for a in msg)
 
 
 def parse_literal_value(msg):
     packet_len = len(msg)
-    print(f"{msg}")
+    logging.debug(f"Parsing literal {msg}")
     binary_string = ""
     while True:
         header, four_bits, msg = msg[0], msg[1:5], msg[5:]
-        print(f"{four_bits:>{packet_len - len(msg)}}")
+        logging.debug(f"{four_bits:>{packet_len - len(msg)}}")
         binary_string += four_bits
         if header == '0':
             break
@@ -16,13 +21,13 @@ def parse_literal_value(msg):
 
 
 def read_operator_packet(msg):
-    print(f"reading operator packet \n{msg}")
+    logging.debug(f"reading operator packet \n{msg}")
     length_type_id, msg = int(msg[0]), msg[1:]
-    print(f"{length_type_id=}")
+    logging.debug(f"{length_type_id=}")
     body_start_idx = 15 - length_type_id * 4
     subpacket_len, body = int(msg[:body_start_idx], 2), msg[body_start_idx:]
-    print(f"subp_len_info:{subpacket_len}")
-    print(f"{body:>{len(msg) + 1}}")
+    logging.debug(f"subp_len_info:{subpacket_len}")
+    logging.debug(f"{body:>{len(msg) + 1}}")
     subpackets = []
     length_read = 0
     while length_read != subpacket_len:
@@ -34,32 +39,51 @@ def read_operator_packet(msg):
                 length_read += initial_len - len(body)
             case 1:
                 length_read += 1
-        print(f"subpkg read:{length_read}/{subpacket_len}")
+        logging.debug(f"subpkg read:{length_read}/{subpacket_len}")
 
     return subpackets, body
 
 
 def read_packet(msg):
-    print(f"reading packet \n{msg}")
+    logging.debug(f"reading packet \n{msg}")
     version, type_id, body = msg[:3], msg[3:6], msg[6:]
-    print(f"version={int(version, 2)}")
-    print(f"type={int(type_id, 2)}")
-    print(f"{body:>{len(msg)}}")
+    logging.debug(f"version={int(version, 2)}")
+    logging.debug(f"type={int(type_id, 2)}")
+    logging.debug(f"{body:>{len(msg)}}")
     if int(type_id, 2) == 4:
         value, left = parse_literal_value(body)
-        print(f"literal ret {value, left}")
+        logging.debug(f"literal ret {value, left}")
         value = int(version, 2)
     else:
         subpackets, left = read_operator_packet(body)
-        value = sum(subpackets) + int(version, 2)
+        value = part1_value_calculator(subpackets, version, type_id)
     return value, left
 
 
-if __name__ == "__main__":
+def part1_value_calculator(subs, vers, _):
+    return sum(subs) + int(vers, 2)
+
+
+def initiate_logger(level=logging.WARNING):
+    logger = logging.getLogger('root')
+    logger.setLevel(level)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(logging.Formatter('%(levelname)s:%(name)s:%(message)s'))
+    logger.addHandler(stream_handler)
+
+
+def main():
     input_file = "input.txt"
     with open(input_file, 'r') as f:
         raw_input = f.read()
-    raw_input = "A0016C880162017C3686B18A3D4780"
+    # raw_input = "A0016C880162017C3686B18A3D4780"
+
+    initiate_logger()
+
     packet = convert_hex_to_bin(raw_input)
     ret, left = read_packet(packet)
     print(f"{ret=}")
+
+
+if __name__ == "__main__":
+    main()
